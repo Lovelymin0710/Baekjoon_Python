7ee74509f1c99e3343ff1b71e381964290a3957d
KIMKYOUNGMIN <waryong1004@naver.com>
2022-05-12 17:29:57 +0900

    백준 문제풀기

---
+++ python_10950.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = []
+
+for i in range(n):
+    a,b = map(int,input().split())
+    lst.append(a+b)
+
+for i in range(n):
+    print(lst[i])        
---
+++ python_2739.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+for i in range(1,10):
+    print(n,'*',i,'=',n*i) #print()함수에 정수와 문자열을 어떻게 같이 쓸까 고민했었음.
+    
+    
